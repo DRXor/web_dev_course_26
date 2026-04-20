1cb546315f7670a566f7eb0709ba472ee6a10ca4
DRXor <horkovadasa843@gmail.com>
2026-04-20 17:25:42 +0000

    move token to env

--- labs/telegram-bot/bot.rb
+++ labs/telegram-bot/bot.rb
@@ -1,7 +1,7 @@
 require 'telegram/bot'
 require_relative 'handlers/matrix_handler'
 
-TOKEN = '8745581870:AAFRAkArBGb1khLNAuSd2TiMu137ff6uhWk'
+TOKEN = ENV['TELEGRAM_BOT_TOKEN']
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
