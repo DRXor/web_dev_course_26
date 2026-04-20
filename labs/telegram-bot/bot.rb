dd441c983eabc2f2e33db5105c72fbc61c9229c3
DRXor <horkovadasa843@gmail.com>
2026-04-20 19:46:32 +0000

    added webrick

--- labs/telegram-bot/bot.rb
+++ labs/telegram-bot/bot.rb
@@ -1,5 +1,21 @@
 require 'telegram/bot'
 require_relative 'handlers/matrix_handler'
+require 'webrick'
+
+Thread.new do
+  server = WEBrick::HTTPServer.new(
+    Port: ENV['PORT'] || 10000,
+    Logger: WEBrick::Log.new(File.open(File::NULL, 'w')),
+    AccessLog: []
+  )
+
+  server.mount_proc '/' do |req, res|
+    res.body = "OK"
+  end
+
+  server.start
+end
+
 
 TOKEN = ENV['TELEGRAM_BOT_TOKEN']
 
